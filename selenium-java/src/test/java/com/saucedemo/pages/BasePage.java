83a50ff75273e9dd6241ff780d65b1a299ea2555
wilarads <wilarads@gmail.com>
2026-06-04 00:05:06 -0400

    feat: add login page tests with page object model

---
+++ selenium-java/src/test/java/com/saucedemo/pages/BasePage.java
@@ -0,0 +1,58 @@
+package com.saucedemo.pages;
+
+import com.saucedemo.config.ConfigReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+
+    protected BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(ConfigReader.getTimeout())
+        );
+    }
+
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    protected WebElement waitForVisibility(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitForClickability(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected void click(By locator) {
+        waitForClickability(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        WebElement element = waitForVisibility(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitForVisibility(locator).getText();
+    }
+
+    protected boolean isDisplayed(By locator) {
+        return waitForVisibility(locator).isDisplayed();
+    }
+
+    protected boolean isEnabled(By locator) {
+        return waitForClickability(locator).isEnabled();
+    }
+}
